ff38ce29a38d44054ee82fcaeea33363e7f2d455
ludaohua <15711476734@163.com>
2022-11-22 11:04:29 +0800

    初始化webpack4,添加sass,image,font处理的loader

---
+++ src/index.js
@@ -0,0 +1,2 @@
+require("./index.scss");
+console.log("测试webpack");
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+// console.log(path.resolve());
+// console.log(path.join(__dirname, "./dist"));
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|sass)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+};
